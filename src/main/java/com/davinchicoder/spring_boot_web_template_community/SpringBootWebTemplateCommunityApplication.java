613dafbdb25c9de1ad3afdcd463064cc374d511c
David <david11jv@gmail.com>
2025-04-17 16:16:12 +0200

    Initial commit

---
+++ src/main/java/com/davinchicoder/spring_boot_web_template_community/SpringBootWebTemplateCommunityApplication.java
@@ -0,0 +1,13 @@
+package com.davinchicoder.spring_boot_web_template_community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebTemplateCommunityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebTemplateCommunityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/davinchicoder/spring_boot_web_template_community/SpringBootWebTemplateCommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.davinchicoder.spring_boot_web_template_community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebTemplateCommunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
